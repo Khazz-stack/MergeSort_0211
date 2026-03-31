38fff5f746e7e99c26d469b393aad4428895f12c
Khazz <thelenaid@gmail.com>
2026-03-31 11:29:12 +0700

    void mergeSort

--- MyklassMergeSort.cpp
+++ MyklassMergeSort.cpp
@@ -30,3 +30,20 @@ void input()
         cin >> arr[i];
     }
 }
+
+void mergeSort(int low, int high)
+{
+    if (low >= high)
+    {
+        return;
+    }
+
+    int mid = (low + high) / 2;
+
+    mergeSort(low, mid);
+    mergeSort(mid + 1, high);
+
+    int i = low;
+    int j = mid + 1;
+    int k = low;
+}
